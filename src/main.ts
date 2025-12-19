85a33d2d484d925241277d38a31b5665d52f859f
Roberto Costa <roberto.araujo339@gmail.com>
2025-12-19 16:28:40 -0300

    add Swagger documentation setup and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 
@@ -11,6 +12,17 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: '*',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('NextWatch API')
+    .setDescription('The NextWatch API description')
+    .setVersion('1.0')
+    .addTag('nextwatch')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
