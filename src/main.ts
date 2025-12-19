783f4fc1cdf1a25ed68852f18f8e1a7521efd3ef
Roberto Costa <roberto.araujo339@gmail.com>
2025-12-19 12:42:47 -0300

    update default port in main.ts from 3000 to 3333

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: '*',
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
