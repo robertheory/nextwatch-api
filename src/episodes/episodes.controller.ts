c8d9ef5b70cf6beae7c1a4fb5eb796aed329cf70
Roberto Costa <roberto.araujo339@gmail.com>
2025-12-20 12:42:10 -0300

    refactor episodes controller and service to replace create and remove methods with markWatched and markUnwatched functionality

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,31 +1,28 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 
 @Controller('watched')
 export class EpisodesController {
   constructor(private readonly episodesService: EpisodesService) {}
 
-  @Post(':showId/:episodeId')
-  create(
-    @Param('showId') showId: string,
-    @Param('episodeId') episodeId: string,
-  ) {
-    return this.episodesService.create({
-      showId: +showId,
-      episodeId: +episodeId,
-    });
-  }
-
   @Get(':showId')
   findAll(@Param('showId') showId: string) {
     return this.episodesService.findAll(+showId);
   }
 
-  @Delete(':showId/:episodeId')
-  remove(
+  @Patch(':showId/:episodeId')
+  markWatched(
     @Param('showId') showId: string,
     @Param('episodeId') episodeId: string,
   ) {
-    return this.episodesService.remove(+showId, +episodeId);
+    return this.episodesService.markWatched(+showId, +episodeId);
+  }
+
+  @Patch(':showId/:episodeId')
+  markUnwatched(
+    @Param('showId') showId: string,
+    @Param('episodeId') episodeId: string,
+  ) {
+    return this.episodesService.markUnwatched(+showId, +episodeId);
   }
 }
--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
+import { ShowStatus } from 'generated/prisma/enums';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEpisodeDto } from './dto/create-episode.dto';
 import { Episode } from './entities/episode.entity';
@@ -11,8 +12,18 @@ import { Episode } from './entities/episode.entity';
 export class EpisodesService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createEpisodeDto: CreateEpisodeDto) {
+  private async create(createEpisodeDto: CreateEpisodeDto) {
     const newEpisodeData = new Episode(createEpisodeDto);
+
+    const showExists = await this.prisma.show.findUnique({
+      where: { showId: newEpisodeData.showId },
+    });
+
+    if (!showExists) {
+      await this.prisma.show.create({
+        data: { showId: newEpisodeData.showId, status: ShowStatus.UNTRACKED },
+      });
+    }
 
     const alreadyExists = await this.prisma.watchedEpisode.findFirst({
       where: {
@@ -44,7 +55,24 @@ export class EpisodesService {
     return episodes;
   }
 
-  async remove(showId: number, episodeId: number) {
+  async markWatched(showId: number, episodeId: number) {
+    const exists = await this.prisma.watchedEpisode.findUnique({
+      where: { showId, episodeId },
+    });
+
+    if (!exists) {
+      await this.create({ showId, episodeId });
+    }
+
+    const updatedEpisode = await this.prisma.watchedEpisode.update({
+      where: { episodeId, showId },
+      data: { watchedAt: new Date() },
+    });
+
+    return updatedEpisode;
+  }
+
+  async markUnwatched(showId: number, episodeId: number) {
     const exists = await this.prisma.watchedEpisode.findUnique({
       where: { showId, episodeId },
     });
@@ -53,8 +81,11 @@ export class EpisodesService {
       throw new NotFoundException('Episode not found.');
     }
 
-    await this.prisma.watchedEpisode.delete({
-      where: { episodeId },
+    const updatedEpisode = await this.prisma.watchedEpisode.update({
+      where: { episodeId, showId },
+      data: { watchedAt: null },
     });
+
+    return updatedEpisode;
   }
 }
